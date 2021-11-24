b4cc8036b3d29393db26b220c85b820d7debd4fd
leoita <leonardolorusso83@gmail.com>
2021-11-24 23:51:05 +0100

    Test mockito void methods throw exception

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-<h1>Mockito project</h1>
-<hr>
+<h1>Mockito project</h1><hr>
 <p>Reference to: <a href = 'https://www.udemy.com/course/mockito3'>Mockito: Next-Level Java Unit Testing</a></p>
-<h2>Matchers Rules</h2>
-<hr>
+<h2>Matchers Rules</h2><hr>
 <ul>
 <li>Use any() for Objects. For primitives, use anyDouble(), anyBoolean() etc.</li>
 <li>
@@ -13,12 +11,15 @@ Use eq() to mix matchers and concrete values:
 For nullable Strings, use any() instead of anyString() as the second one doesn't cover the null case.
 </li>
 </ul>
-<h2>Spy vs Mock</h2>
-<hr>
+<h2>Spy vs Mock</h2><hr>
 <h4>MOCK:</h4> 
 dummy object with no real logic and always returns the default value
 <br><i>when(mock.method()).thenReturn()</i>
-
 <h4>SPY:</h4>
 partial mock, a real object with real logic that we can modify
 <br><i>doReturn().when(spy.method())</i>
+<h2>Mock throw exception</h2><hr>
+<h4>Void methods:</h4>
+<i>doThrow(Exception.class).when(MockObject).mockMethod();</i>
+<h4>Other methods:</h4>
+<i>when(MockObject.MockMethod()).thenThrow(Exception.class);</i>
---
+++ src/test/java/com/leoita/hotel/booking/Test09MockitoVoidMethods.java
@@ -0,0 +1,67 @@
+package com.leoita.hotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class Test09MockitoVoidMethods {
+
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private Booking bookingMock;
+    private MailSender mailSenderMock;
+
+
+    @BeforeEach
+    void setup() {
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.mailSenderMock = mock(MailSender.class);
+        this.bookingMock = mock(Booking.class);
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock,
+                bookingMock, mailSenderMock);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMailNotReady() {
+
+        //given
+        BookingRequest bookingRequest = new BookingRequest("1",
+                LocalDate.of(2021, 12, 12),
+                LocalDate.of(2021, 12, 16),
+                2, false);
+
+        doThrow(AvailabilityException.class).when(mailSenderMock).sendBookingConfirmation(any());
+        //when
+        Executable executable = () -> bookingService.makeBooking(bookingRequest);
+
+        // then
+        assertThrows(AvailabilityException.class, executable);
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenMailNotReady() {
+
+        //given
+        BookingRequest bookingRequest = new BookingRequest("1",
+                LocalDate.of(2021, 12, 12),
+                LocalDate.of(2021, 12, 16),
+                2, false);
+
+        doNothing().when(mailSenderMock).sendBookingConfirmation(any());
+        //when
+        String id = bookingService.makeBooking(bookingRequest);
+
+        // then
+        //do not throw exceptions
+        verify(mailSenderMock, times(1)).sendBookingConfirmation(any());
+        assertNull(id);
+    }
+}
